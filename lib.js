d736ee1c5b804159be055782e039f74a8a8198fc
kukukukikii <shawnnq@gmail.com>
2013-03-28 11:34:16 +0800

    Update lib.js
    disable select

--- lib.js
+++ lib.js
@@ -1,1 +1,11 @@
 //a file of library
+
+//plugin of disable select
+(function($){
+  $.fn.disableSelection = function() {
+    return this
+    .attr('unselectable', 'on')
+    .css('user-select', 'none')
+    .on('selectstart', false);
+  };
+})(jQuery);
